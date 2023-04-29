ac651b5aa934b60a8d9e492577440e39254874b1
hasiqbal <85685078+hasiqbal@users.noreply.github.com>
2023-04-29 13:34:40 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello AWS lambda from my aws-lambda-nodejs-github")
+    body: JSON.stringify("Hello AWS lambda fom s3-Lambda, how are you?")
   };
   return response;
 }
